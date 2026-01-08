b895e0601ed3b85788808b38294b9c8612aa1cfd
alekoza02 <alessio.cimma@gmail.com>
2026-01-08 19:54:42 +0100

    Updated compilation process. Unification of the writing process. Added 'debug' mode by showing log errors

--- main.py
+++ main.py
@@ -1,81 +1,81 @@
 import subprocess
-import time
 
 class ReLogter:
     
-    def __init__(self, file_name="output", compile=True, rm_garbage=True, live_update=False):
+    def __init__(self, file_name="output", compile=True, rm_garbage=True, live_update=False, show_errors=False):
 
         self.file_name = file_name
         self.compile = compile        
         self.rm_garbage = rm_garbage
         self.live_update = live_update
+        self.show_errors = show_errors
         
         self.__output_string = ""
 
 
     def initialize_document(self):
-        if self.live_update:
-            with open(f"{self.file_name}.tex", "w") as f:
-                print(r"\documentclass{article}", "\n", r"\begin{document}", file=f)
-        else:
-            self.__output_string = self.__output_string + r"\documentclass{article}" + "\n" + r"\begin{document}" + "\n"
+        message = r"\documentclass{article}" + "\n" + r"\begin{document}" + "\n"
+        self.__update_buffer(message, file_open_mode="w")
 
 
-    def write_message(self, message):
-        if self.live_update:
-            with open(f"{self.file_name}.tex", "a") as f:
-                print(fr"{message}", file=f)
-        else:
-            self.__output_string = self.__output_string + message + "\n"
+    def write_message(self, message, noindent=False):
+        if noindent:
+            message = r"\noindent " + message
+        self.__update_buffer(message)
+
+
+    def write_section(self, section_name):
+        message = fr"\section{{{section_name}}}"
+        self.__update_buffer(message)
 
 
     def close_document(self):
-        if self.live_update:
-            with open(f"{self.file_name}.tex", "a") as f:
-                print(r"\end{document}", file=f)
-        else:
-            self.__output_string = self.__output_string + r"\end{document}"
+        message = r"\end{document}"
+        self.__update_buffer(message)
+        
+        if not self.live_update:
             with open(f"{self.file_name}.tex", "w") as f:
                 print(f"{self.__output_string}", file=f)
 
 
+    def __update_buffer(self, message, file_open_mode="a"):
+        if self.live_update:
+            with open(f"{self.file_name}.tex", file_open_mode) as f:
+                print(f"{message}", file=f, end="")
+        else:
+            self.__output_string = self.__output_string + message
+
+
     def compile_into_pdf(self):
-        subprocess.run(
-            ["pdflatex", "-interaction=nonstopmode", f"{self.file_name}.tex"],
-            check=True
+        mode = "-interaction=nonstopmode" if self.show_errors else "-interaction=batchmode"
+
+        result = subprocess.run(
+            ["pdflatex", "-halt-on-error", mode, f"{self.file_name}.tex"],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            text=True
         )
 
+        if self.show_errors:
+            print(result.stdout)
+            print(result.stderr)
 
-    def remove_aux_file(self, additional_wait_time=5):
-        time.sleep(additional_wait_time)
         subprocess.run(
-            "rm -f *.log *.aux *.fdb_latexmk *.fls *.log *.gz",
-            shell=True,
+            ["latexmk", "-c", "-silent"],
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.DEVNULL,
             check=True
         )
 
 
 if __name__ == "__main__":
     
-    logger1 = ReLogter("output_live", live_update=True)
-    logger2 = ReLogter("output_no_live", live_update=False)
+    logger = ReLogter("log", show_errors=True)
+    
+    logger.initialize_document()
+    logger.write_section("Benvenuto")
+    logger.write_message("Ciao a Ale.\n\n")
+    logger.write_message("Questo è un Messaggio automatico", noindent=True)
+    logger.close_document()
 
-    logger1.initialize_document()
-    logger1.write_message("Ciao a tutti.")
-    logger1.write_message("Messaggio automatico")
-    logger1.close_document()
-
-    logger1.compile_into_pdf()
-    logger1.remove_aux_file()
-
-
-    logger2.initialize_document()
-    time.sleep(5)
-    logger2.write_message("Ciao a tutti.")
-    time.sleep(5)
-    logger2.write_message("Messaggio automatico")
-    time.sleep(5)
-    logger2.close_document()
-
-    logger2.compile_into_pdf()
-    logger2.remove_aux_file()
+    logger.compile_into_pdf()
